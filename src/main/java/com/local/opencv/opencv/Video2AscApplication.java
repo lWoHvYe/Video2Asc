945efcd0017e6d0da6c4bfec244018792715b587
lWoHvYe <39266195+lWoHvYe@users.noreply.github.com>
2019-05-02 20:20:19 +0800

    视频转ASC字符，初版代码

---
+++ src/main/java/com/local/opencv/opencv/Video2AscApplication.java
@@ -0,0 +1,13 @@
+package com.local.opencv.opencv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Video2AscApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Video2AscApplication.class, args);
+    }
+
+}
